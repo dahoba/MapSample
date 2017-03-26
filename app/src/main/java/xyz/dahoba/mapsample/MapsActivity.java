d6a3b0e335f3f3709c2e94653a6b85a04f220631
Siritas Suvadhanabhakdi <dahoba@gmail.com>
2017-03-27 02:03:52 +0700

    - Add google maps API key - Add codes to get last location and make a marker at that

--- app/src/main/java/xyz/dahoba/mapsample/MapsActivity.java
+++ app/src/main/java/xyz/dahoba/mapsample/MapsActivity.java
@@ -1,8 +1,17 @@
 package xyz.dahoba.mapsample;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.PermissionChecker;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,9 +19,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    protected final String TAG = "MapsActivity";
 
     private GoogleMap mMap;
+    private GoogleApiClient googleApiClient;
+    private Location lastLocation;
+
+    public static final int MY_REQUST_PERMISSION = 1;
 
 
     @Override
@@ -24,6 +41,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //initiate GoogelAPIClient
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
     }
 
     /**
@@ -43,7 +82,53 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
     }
 
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (PermissionChecker.PERMISSION_GRANTED != PermissionChecker.checkSelfPermission(this, ACCESS_FINE_LOCATION)) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, ACCESS_FINE_LOCATION)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{ACCESS_FINE_LOCATION}, MY_REQUST_PERMISSION);
+            }
+        } else {
+            getLastLocation();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private void getLastLocation() {
+        if (PermissionChecker.PERMISSION_GRANTED != PermissionChecker.checkSelfPermission(this, ACCESS_FINE_LOCATION)) {
+            Log.w(TAG, "No access location granted!");
+            return;
+        }
+        lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        if (lastLocation != null) {
+            Log.v(TAG, "got location");
+            updateMap();
+        } else {
+            Log.v(TAG, "last location was null!");
+        }
+    }
+
+    private void updateMap() {
+        Log.v(TAG, "updateMap...");
+        if (lastLocation != null) {
+            LatLng lastLoc = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(lastLoc).title("Marker of my location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(lastLoc));
+        }
+    }
 }
